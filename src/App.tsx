9cf30fdab08f8c6d42fbbfd6dc8c31c05d597a36
Kevindo28012003 <kietdo@usf.edu>
2025-09-28 07:21:18 -0400

    UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,37 @@
-/**
- * Copyright 2024 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+import { AuthProvider, useAuth } from './contexts/AuthContext';
+import Authentication from './components/Authentication';
+import FinancialDashboard from './components/FinancialDashboard';
+import './App.css';
 
-import { useRef, useState } from "react";
-import "./App.scss";
-import { LiveAPIProvider } from "./contexts/LiveAPIContext";
-import { Altair } from "./components/altair/Altair";
-import ControlTray from "./components/control-tray/ControlTray";
-import cn from "classnames";
-import { LiveClientOptions } from "./types";
+function AppContent() {
+  const { user, loading } = useAuth();
 
-const API_KEY = process.env.REACT_APP_GEMINI_API_KEY as string;
-if (typeof API_KEY !== "string") {
-  throw new Error("set REACT_APP_GEMINI_API_KEY in .env");
+  if (loading) {
+    return (
+      <div style={{ 
+        display: 'flex', 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        height: '100vh',
+        backgroundColor: '#000000'
+      }}>
+        <div style={{ color: '#00FF99' }}>Loading...</div>
+      </div>
+    );
+  }
+
+  if (!user) {
+    return <Authentication />;
+  }
+
+  return <FinancialDashboard />;
 }
 
-const apiOptions: LiveClientOptions = {
-  apiKey: API_KEY,
-};
-
 function App() {
-  // this video reference is used for displaying the active stream, whether that is the webcam or screen capture
-  // feel free to style as you see fit
-  const videoRef = useRef<HTMLVideoElement>(null);
-  // either the screen capture, the video or null, if null we hide it
-  const [videoStream, setVideoStream] = useState<MediaStream | null>(null);
-
   return (
-    <div className="App">
-      <LiveAPIProvider options={apiOptions}>
-        <div className="streaming-console">
-          <main>
-            <div className="main-app-area">
-              {/* APP goes here */}
-              <Altair />
-              <video
-                className={cn("stream", {
-                  hidden: !videoRef.current || !videoStream,
-                })}
-                ref={videoRef}
-                autoPlay
-                playsInline
-              />
-            </div>
-
-            <ControlTray
-              videoRef={videoRef}
-              supportsVideo={true}
-              onVideoStreamChange={setVideoStream}
-            >
-              {/* put your own buttons here */}
-            </ControlTray>
-          </main>
-        </div>
-      </LiveAPIProvider>
-    </div>
+    <AuthProvider>
+      <AppContent />
+    </AuthProvider>
   );
 }
 
